a768a6dacfabd8f7ea6a818a9a273c2536040b5b
anelson92 <115906480+anelson92@users.noreply.github.com>
2023-02-03 09:38:38 -0600

    Add files via upload

---
+++ routes/api/category-routes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try { 
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  
+});
+
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  }).then((newCategory)=> {
+    res.json(newCategory);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then((updatedCategory) => {
+    res.status(200).json(updatedCategory);
+  }).catch((err) => res.status(500).json(err))
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((deletedCategory) => {
+    res.status(200).json(deletedCategory);
+  }).catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
